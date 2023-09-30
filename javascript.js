9909708f5be94ab5d9f16781eb2c5e6d2528cad1
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-09-30 12:11:44 -0700

    done

--- javascript.js
+++ javascript.js
@@ -1,13 +1,69 @@
 // 1.1 Assignment: Where do you put JavaScript?
 
 // 1.2 Assignment: Variables
+var john; 
+var john_firstName= "John";
+var  john_lastName= "Smith";
+var  john_age = "25";
+var  john_home= "United Kingdom"
 
+
+console.log(john_firstName)
+console.log(john_lastName)
+console.log(john_age)
+console.log(john_home)
 // 1.3 Assignment: Variable Declaration & Initialization
+var a = 3;
+var b = 5;
+var c = 7;
+var d;
+var e = 9;
 
+console.log(e)
+console.log(b)
+console.log(a)
+console.log(d)
+console.log(c)
 // 1.4 Assignment: Complete your statements
+x = 2;
+y = 3;
 
+z = x + y;
 // 1.5 Assignment: Brackets, Braces and Parentheses
+var a = function () {
+    alert('Statement 1');
+    alert('Statement 2');
+}
 
+console.log(a)
 // 1.6 Assignment: Data Types
+/*Assign the following to a different letter from the alphabet:
+your name
+your age
+you like coffee
+make a comment with the data type for each one
+Output the results with console.log statements.*/
 
+
+var a = ("Andrea");
+var s = ("Shepherd");
+var o = (48);
+var d = (true);
+
+console.log(a) //a + s are strings
+console.log(s) 
+console.log(o) //o is a number
+console.log(d) //d is boolean
 // 1.7 Assignment: Practicing JavaScript basics
+var l = (2 + 2)
+console.log(l)
+
+var m = (3 + 4 + 5)
+console.log(m)
+
+var a = (4);
+var b = (3);
+var c = (5);
+var area = (a + b / 2)
+console.log(a + b / 2)
+console.log(area)
